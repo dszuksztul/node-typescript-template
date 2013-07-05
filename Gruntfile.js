edcda48344a3575644af2db7f89adae0dbcc0360
Dariusz Szuksztul <Dariusz.Szuksztul@ydp.eu>
2013-07-05 13:39:34 +0200

    adding template for tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,11 @@ module.exports = function(grunt) {
 	var BASE_DIR = "./";
 	var SRC_TS_DIR = BASE_DIR + "src/main/ts/";
 	var DST_TS_DIR = SRC_TS_DIR;
+	var SRC_TEST_TS_DIR = BASE_DIR + "src/test/ts/";
+	var DST_TEST_TS_DIR = SRC_TEST_TS_DIR;
+	var JASMINE_SPEC_EXT = '*.spec.js';
 	var TS_EXT = "*.ts";
+	var JASMINE_REPORT_DIR = BASE_DIR+"report/";
 
 	grunt.initConfig({
 		jshint: {
@@ -24,12 +28,27 @@ module.exports = function(grunt) {
 					fullSourceMapPath: false,
 					declaration: false
 				}
+			},
+			test: {
+				src: [SRC_TEST_TS_DIR + TS_EXT],
+				dest: DST_TEST_TS_DIR,
+				options: {
+					target: 'es3',
+					base_path: SRC_TEST_TS_DIR,
+					sourcemap: true,
+					fullSourceMapPath: false,
+					declaration: false
+				}
 			}
 		},
 		watch: {
 			sources: {
 				files: [SRC_TS_DIR + TS_EXT],
 				tasks: ['generate']
+			},
+			test: {
+				files: [SRC_TS_DIR + TS_EXT, SRC_TEST_TS_DIR+TS_EXT],
+				tasks: ['test']
 			}
 		},
 		execute: {
@@ -37,8 +56,19 @@ module.exports = function(grunt) {
 				src: [DST_TS_DIR+"main.js"]
 			}
 		},
+        jasmine_node: {
+		    projectRoot: DST_TEST_TS_DIR,
+		    requirejs: false,
+		    forceExit: false,
+		    jUnit: {
+		      report: true,
+		      savePath : JASMINE_REPORT_DIR,
+		      useDotNotation: true,
+		      consolidate: true
+		    }
+        },
 		clean: {
-			build: [SRC_TS_DIR + "*.js*"],
+			build: [SRC_TS_DIR + "*.js*", SRC_TEST_TS_DIR + "*.js*"],
 			node_modules: ["node_modules"]
 		}
 	});
@@ -48,9 +78,12 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-typescript');
 	grunt.loadNpmTasks('grunt-execute');
+	grunt.loadNpmTasks('grunt-jasmine-node');
 
 	grunt.registerTask('generate', ['jshint', 'typescript']);
 	grunt.registerTask('watchSources', ['generate', 'watch:sources']);
+	grunt.registerTask('watchTests', ['test', 'watch:test']);
 	grunt.registerTask('default', ['watchSources']);
 	grunt.registerTask('exec', ['generate', "execute"]);
+	grunt.registerTask('test', ['typescript', "jasmine_node"]);
 };
--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# typescript-template
+# node-typescript-template
 
 ## Objective
-This Gist provides sample typescript project template for Node.js.
-Includes gruntfile for basic project preparation.
+This project provides sample typescript project template for Node.js.
+Includes gruntfile for basic project preparation and for running Jasmine tests.
+
+Usage is free.
+includes work from: [DefinitelyTyped](https://github.com/borisyankov/DefinitelyTyped).
 
 ## Usage
 Execute:
- npm install
- grunt exec
 
-Add your own source files into src/main/ts.
-Add typescript definitiopn files into lib/ directory.
+    npm install  
+    grunt exec
+
+or
+
+    grunt test
+
+Add your own source files into src/main/ts.  
+Add your own test files into src/test/ts.  
+Add typescript definitiopn files into lib/ directory.  
 
 Available grunt tasks:
- exec - execute sample program
- generate - build js files from ts
- watchSources - watch file changes and autmaticaly trigger js build
- clean:build - clean build javascript files
- clean:node_modules - clean downloaded node_modules
+ * exec - execute sample program
+ * generate - build js files from ts
+ * test - run jasmine test
+ * watchTests - watch file changes and autmaticaly trigger build and tests
+ * watchSources - watch file changes and autmaticaly trigger js build
+ * clean:build - clean build javascript files
+ * clean:node_modules - clean downloaded node_modules
  
-
-## Developing
-
